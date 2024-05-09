ebabee0ff376eb56762525318abfd89a6ce50bc8
alexsandroferreira <ferreira.alex@outlook.com>
2024-05-09 19:04:09 -0300

    feat: add route get-event

--- src/https/controllers/events/routes.ts
+++ src/https/controllers/events/routes.ts
@@ -87,4 +87,31 @@ export async function eventsRoutes(app: FastifyInstance) {
       })
     },
   )
+
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/events/:eventId',
+    {
+      schema: {
+        params: z.object({
+          eventId: z.string().uuid(),
+        }),
+        response: {},
+      },
+    },
+    async (request, reply) => {
+      const { eventId } = request.params
+
+      const event = await prisma.event.findUnique({
+        where: {
+          id: eventId,
+        },
+      })
+
+      if (event === null) {
+        throw new Error('Event not found')
+      }
+
+      return reply.send({ event })
+    },
+  )
 }
