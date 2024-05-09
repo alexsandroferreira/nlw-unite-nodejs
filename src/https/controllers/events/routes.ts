8c7da915266cbf3d88c6528e165d5ac1dbcf5de8
alexsandroferreira <ferreira.alex@outlook.com>
2024-05-09 10:04:35 -0300

    feat(migrations): add uniqueness on table attendee

--- src/https/controllers/events/routes.ts
+++ src/https/controllers/events/routes.ts
@@ -1,5 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
 
 import { CreateEvent } from './create-event-controller'
 import { CreateEventsSchema, EventResponseSchema } from './create-event-schema'
@@ -14,4 +17,38 @@ export async function eventsRoutes(app: FastifyInstance) {
     },
     handler: CreateEvent,
   })
+
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/events/:eventId/attendees',
+    {
+      schema: {
+        body: z.object({
+          name: z.string().min(4),
+          email: z.string().email(),
+        }),
+        params: z.object({
+          eventId: z.string().uuid(),
+        }),
+        response: {
+          201: z.object({
+            message: z.string(),
+            attendeeId: z.number(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { eventId } = request.params
+      const { name, email } = request.body
+
+      const attendee = await prisma.attendee.create({
+        data: { name, email, eventId },
+      })
+
+      reply.status(201).send({
+        message: 'registro no evento concluido com sucesso.',
+        attendeeId: attendee.id,
+      })
+    },
+  )
 }
