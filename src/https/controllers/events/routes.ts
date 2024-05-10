1a95eeb8270b95258feb303a7be5de45d56758f2
alexsandroferreira <ferreira.alex@outlook.com>
2024-05-10 14:27:40 -0300

    feat: update route badge

--- src/https/controllers/events/routes.ts
+++ src/https/controllers/events/routes.ts
@@ -151,7 +151,16 @@ export async function eventsRoutes(app: FastifyInstance) {
         params: z.object({
           attendeeId: z.coerce.number().int(),
         }),
-        response: {},
+        response: {
+          200: z.object({
+            badge: z.object({
+              name: z.string(),
+              email: z.string().email(),
+              eventTitle: z.string(),
+              checkInURL: z.string().url(),
+            }),
+          }),
+        },
       },
     },
     async (request, reply) => {
@@ -176,11 +185,17 @@ export async function eventsRoutes(app: FastifyInstance) {
         throw new Error('Attendeee not found')
       }
 
+      const baseURL = `${request.protocol}://${request.hostname}`
+
+      console.log(baseURL)
+      const checkInURL = new URL(`/attendees/${attendeeId}/check-in`, baseURL)
+
       return reply.send({
-        attendee: {
+        badge: {
           name: attendee.name,
           email: attendee.email,
           eventTitle: attendee.event.title,
+          checkInURL: checkInURL.toString(),
         },
       })
     },
