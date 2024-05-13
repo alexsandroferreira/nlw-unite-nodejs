356e570e4d3e2295eaad28b1bac23b5a83165fd2
alexsandroferreira <ferreira.alex@outlook.com>
2024-05-13 13:42:51 -0300

    feat: add route attendees

--- src/https/controllers/events/routes.ts
+++ src/https/controllers/events/routes.ts
@@ -200,4 +200,110 @@ export async function eventsRoutes(app: FastifyInstance) {
       })
     },
   )
+
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/attendees/:attendeeId/check-in',
+    {
+      schema: {
+        params: z.object({
+          attendeeId: z.coerce.number().int(),
+        }),
+        response: { 201: z.null() },
+      },
+    },
+    async (request, reply) => {
+      const { attendeeId } = request.params
+
+      const attendeeCheckIn = await prisma.checkIn.findUnique({
+        where: {
+          attendeeId,
+        },
+      })
+
+      if (attendeeCheckIn !== null) {
+        throw new Error('Attendee already checked in.')
+      }
+
+      await prisma.checkIn.create({
+        data: {
+          attendeeId,
+        },
+      })
+
+      return reply.status(201).send()
+    },
+  )
+
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/events/:eventId/attendees',
+    {
+      schema: {
+        params: z.object({
+          eventId: z.string().uuid(),
+        }),
+        querystring: z.object({
+          query: z.string().nullish(),
+          pageIndex: z.string().nullable().default('0').transform(Number),
+        }),
+        response: {
+          200: z.object({
+            attendees: z.array(
+              z.object({
+                id: z.number(),
+                name: z.string(),
+                email: z.string().email(),
+                createdAt: z.date(),
+                checkedInAt: z.date().nullable(),
+              }),
+            ),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { eventId } = request.params
+      const { pageIndex, query } = request.query
+
+      const attendees = await prisma.attendee.findMany({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          createdAt: true,
+          checkIn: {
+            select: {
+              createdAt: true,
+            },
+          },
+        },
+        where: query
+          ? {
+              eventId,
+              name: {
+                contains: query,
+              },
+            }
+          : {
+              eventId,
+            },
+        take: 10,
+        skip: pageIndex * 10,
+        orderBy: {
+          createdAt: 'desc',
+        },
+      })
+
+      reply.send({
+        attendees: attendees.map((attendee) => {
+          return {
+            id: attendee.id,
+            name: attendee.name,
+            email: attendee.email,
+            createdAt: attendee.createdAt,
+            checkedInAt: attendee.checkIn?.createdAt ?? null,
+          }
+        }),
+      })
+    },
+  )
 }
